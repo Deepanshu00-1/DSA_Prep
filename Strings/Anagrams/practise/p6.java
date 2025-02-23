ac8035e1eace9634548282f15a31344ecac23aef
Deepanshu <krrohan129@gmail.com>
2025-02-23 23:20:15 +0530

    .

---
+++ Strings/Anagrams/practise/p6.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class p6 {
+    public static void main(String[] args){
+        System.out.println(isAnagram("listen", "silent"));
+    }
+    public static boolean isAnagram(String a, String b){
+        if(a.length() != b.length()) return false;
+        char[] aArr = a.toCharArray();
+        char[] bArr = b.toCharArray();
+        Arrays.sort(aArr);
+        Arrays.sort(bArr);
+        return Arrays.equals(aArr, bArr);
+    }
+}
